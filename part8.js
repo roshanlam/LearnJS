8f8c3265691e5dc3814a5152c4a1f2ef9354f941
Roshan Lamichhane <33913826+roshanlam@users.noreply.github.com>
2020-01-20 14:56:02 -0500

    Update part8.js

--- part8.js
+++ part8.js
@@ -3,9 +3,16 @@
 let student = {
 	first: 'Roshan',
 	last: 'Lamichhane', 
-	age: 25 // Not real age
-	height: 72 // in inches
+	age: 25, // Not real age
+	height: 72, // in inches
+	studentInfo: function(){
+		return this.first + '\n' + this.last + '\n' this.age;
+	}
 };
 
-console.log(student.first);
-console.log(student.last);
+// console.log(student.first);
+// console.log(student.last);
+
+student.age++;
+console.log(student.age);
+console.log(student.studentInfo());
