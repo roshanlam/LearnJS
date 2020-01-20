599fb76505bd0c89f32edd2be5250d1a9c380400
Roshan Lamichhane <33913826+roshanlam@users.noreply.github.com>
2020-01-20 14:56:29 -0500

    Create part9.js

---
+++ part9.js
@@ -0,0 +1,13 @@
+// conditionals, control flows (if else)
+// 18-35 is my target demographic 
+// && AND
+// || OR
+var age = prompt('what is your age?');
+
+if ( (age >= 18) && (age <= 35)){
+	status = 'target demo';
+	console.log(status);
+} else {
+	status = 'not my audience';
+	console.log(status);
+}
