3a640414f170167b12a9dfbcbe25cfb8eec0dca3
Roshan Lamichhane <33913826+roshanlam@users.noreply.github.com>
2020-01-20 14:34:28 -0500

    Create part2.js

---
+++ part2.js
@@ -0,0 +1,13 @@
+alert('This is a simple alert');
+
+// variables 
+var a = 'this is variable a ';
+console.log(a);
+
+var someNumber = 28;
+console.log(someNumber);
+
+
+var name = prompt('what is your name?');
+
+document.getElementById('sometext').innerHTML = name;
