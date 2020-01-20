154b9707cb0132fb1d830440350d57f521d7e589
Roshan Lamichhane <33913826+roshanlam@users.noreply.github.com>
2020-01-20 14:56:43 -0500

    Create part10.js

---
+++ part10.js
@@ -0,0 +1,17 @@
+ // Switch statements
+    // differentiate between weekday vs. weekend
+   // day 0 --> Sunday   --> weekend
+  //  day 6 --> Saturday --> weekend
+ //   day 5 --> Friday --> weekend 
+//    day 4 --> Thursday --> weekday
+
+switch(2){
+	case 0:
+	text = 'weekend';
+	break;
+	case 5:
+	text = 'weekend';
+	break;
+// complete this program
+}
+console.log(text);
