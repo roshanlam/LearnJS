ab23876fbaff3580430e1af698e1112b2da07ece
Roshan Lamichhane <33913826+roshanlam@users.noreply.github.com>
2020-01-20 14:35:05 -0500

    Create part3.js

---
+++ part3.js
@@ -0,0 +1,18 @@
+// Numbers 
+
+var num1 = 5;
+
+// Increment num1 by 1
+num1++ 
+
+// Decrement num1 by 1
+num1 --
+
+console.log(num1);
+
+// Divide, mutiply *, remainder %
+console.log(num1 / 6);
+
+// Either Increment or Decrement by 10
+num1 += 10;
+console.log(num1);
