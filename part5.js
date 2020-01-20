a901dd5e15ac62fed969d41e8a38b8f0778366dc
Roshan Lamichhane <33913826+roshanlam@users.noreply.github.com>
2020-01-20 14:36:07 -0500

    Create part5.js

---
+++ part5.js
@@ -0,0 +1,12 @@
+// while loops
+var num = 0;
+while (num < 100){
+	num += 1;
+	console.log(num)
+}
+
+// now let's the same thing again but this time in a for loop
+ 
+ for (let num = 0; num <= 100; num++){
+ 	console.log(num)
+ }
