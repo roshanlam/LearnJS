a1d4abe3ebb602c636ad526fc2cd39b9e00915f0
Roshan Lamichhane <33913826+roshanlam@users.noreply.github.com>
2020-01-20 14:37:14 -0500

    Create part8.js

---
+++ part8.js
@@ -0,0 +1,11 @@
+// Objects in javascript
+
+let student = {
+	first: 'Roshan',
+	last: 'Lamichhane', 
+	age: 25 // Not real age
+	height: 72 // in inches
+};
+
+console.log(student.first);
+console.log(student.last);
